14cacf3784a1a8b6476df248d8d0fd53c92d0230
Dima Kostrub <mail@freelook.info>
2017-04-24 18:20:00 +0000

    filter topics

--- plugin/index.js
+++ plugin/index.js
@@ -4,12 +4,66 @@
 
     var plugin = {};
     var async = module.parent.require('async');
-    var Controllers = module.parent.require("./controllers")
+    var validator = module.parent.require('validator');
+    var controllers = module.parent.require("./controllers");
+    var categories = module.parent.require("./categories");
+    var meta = module.parent.require('./meta');
+    var user = module.parent.require('./user');
+    var categoriesByLangs = {
+        ru: 'RU',
+        en: 'EN',
+        de: 'DE'
+    };
 
-    plugin.pageView = function(data) {
-        data.req.params.topic_index = '1';
-        data.req.params.category_id = '5';
-        Controllers.category.get(data.req, data.res, data.next);
+    plugin.homepageGetCategories = function(data) {
+        var lang;
+        async.waterfall([
+            function(next) {
+                if (!data.req.user) {
+                    return next(null, meta.config);
+                }
+                user.getSettings(data.req.uid, next);
+            },
+            function(settings, next) {
+                lang = (data.req.query.lang ? validator.escape(String(data.req.query.lang)) : null) || settings.userLang || settings.defaultLang || categoriesByLangs.ru.toLowerCase();
+                categories.getCategoriesByPrivilege('cid:0:children', data.req.uid, 'find', next);
+            },
+            function(categoriesData, next) {
+                var categoryName = categoriesByLangs[lang.split('-')[0].toLowerCase()] || categoriesByLangs.ru;
+                var categoryData = categoriesData.find(function(category) {
+                    return category.name === categoryName;
+                });
+                data.req.params.topic_index = '1';
+                data.req.params.category_id = categoryData.cid;
+                data.req.params.slug = categoryData.name.toLowerCase();
+                controllers.category.get(data.req, data.res, next);
+            }
+        ], data.next);
+    };
+
+    plugin.filterTopicsPrepare = function(data, callback) {
+        var sets = typeof data.set === "string" ? [data.set] : data.set;
+        categories.getChildren([data.cid], data.uid, function(err, children) {
+            if (err) {
+                return callback(err, data);
+            }
+
+            function addChildren(children) {
+                if (children && children.length) {
+                    children.forEach(function(child) {
+                        sets.push("cid:" + child.cid + ":tids");
+                        if (child.children && child.children.length) {
+                            addChildren(child.children);
+                        }
+                    });
+                }
+            }
+            if (children && children[0]) {
+                addChildren(children[0]);
+            }
+            data.set = sets;
+            return callback(null, data);
+        });
     };
 
     module.exports = plugin;
