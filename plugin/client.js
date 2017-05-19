c6ea6a514b1122b259084c8aae820859f3ef349f
Dima Kostrub <mail@freelook.info>
2017-05-19 22:37:57 +0000

    client cache

--- plugin/client.js
+++ plugin/client.js
@@ -12,11 +12,53 @@ define('fli/client', ['components', 'forum/topic/images'], function(components, 
         });
     };
 
+    FLI.cache = function() {
+        $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+            var done = originalOptions.success || $.noop,
+                url = originalOptions.url;
+            options.beforeSend = function() {
+                var proceed = true;
+                try {
+                    var item = sessionStorage.getItem(url);
+                    if (item) {
+                        var jsonItem = JSON.parse(item);
+                        if (jsonItem.expire > Date.now()) {
+                            done(jsonItem.data);
+                            proceed = false;
+                        }
+                        else {
+                            sessionStorage.removeItem(url);
+                        }
+                    }
+                }
+                catch (e) {
+                    proceed = true;
+                }
+                return proceed;
+            };
+            options.success = function(data, textStatus) {
+                if (data) {
+                    try {
+                        sessionStorage.setItem(url, JSON.stringify({
+                            data: data,
+                            expire: Date.now() + 60000
+                        }));
+                    }
+                    catch (e) {
+                        console.log('Can not cache:', data);
+                    }
+                }
+                done(data);
+            };
+        });
+    };
+
     return FLI;
 });
 
 $(document).ready(function() {
     require(['fli/client'], function(fli) {
+        fli.cache();
         fli.init();
     });
 });
